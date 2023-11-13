8ccac35be83d43e4f1b3255bcf655a6e540f2b1f
Jameson <james.bradlee@telenor.no>
2023-11-13 09:27:59 +0100

    Update caching

--- src/downloadAndInstall.ts
+++ src/downloadAndInstall.ts
@@ -1,8 +1,10 @@
 import {
 	addPath,
+	annotation,
 	debug,
 	endGroup,
 	group,
+	notice,
 	panic,
 	randomDir,
 	randomPath,
@@ -16,26 +18,27 @@ import { readdirSync } from "node:fs";
 async function downloadAndInstall(version: string) {
 	const os = sysType();
 	const arch = sysArch();
-
 	if (os !== "Linux") panic("Operative system %o is not supported!", os);
-
-	debug("Already available versions %o", tc.findAllVersions("IntelliJ-IDEA-C"));
-
+	const cacheHit = tc.find("ideac", version, arch) as string | undefined;
+	if (cacheHit) {
+		await addPath(cacheHit);
+		notice(
+			`${version} (cached)`,
+			annotation({ title: "IntelliJ Community Edition" }),
+		);
+		return;
+	}
 	const url = downloadUrl(version, os, arch);
-
 	const tarballPath = await tc.downloadTool(url, randomPath());
 	group("Extracting contents");
 	const dir = await tc.extractTar(tarballPath, await randomDir());
 	endGroup();
 	const pathToIdea = join(dir, readdirSync(dir)[0], "bin");
-
 	await addPath(pathToIdea);
-
 	debug("IntelliJ IDEA has been installed at %o", dirname(pathToIdea));
-
-	const cacheDir = await tc.cacheDir(dir, "IntelliJ-IDEA-C", version, arch);
-
+	const cacheDir = await tc.cacheDir(dir, "ideac", version, arch);
 	debug("Cached to %o", cacheDir);
+	notice(`${version}`, annotation({ title: "IntelliJ Community Edition" }));
 }
 
 export default downloadAndInstall;
